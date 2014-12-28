d2e2891bbab23c8e1a27984adfe0947f639f4b8a
Michael Little <bio595@gmail.com>
2014-12-28 20:15:34 +1300

    logging

--- install-script.js
+++ install-script.js
@@ -7,6 +7,7 @@ var User = mongoose.model('User', require('./models/user'));
 
 User.register(new User({ username: 'Mike'}), process.env.BLAG_ADMIN_PASSWORD || 'password', function(err){
 	if(err) console.log(err);
+	else console.log('Succesfully create admin user');
 
 	process.exit(0);
 });    
